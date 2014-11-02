4600d8fcb9e949f4de82083b10f118b771c1af6f
Federico Tomassetti <f.tomassetti@gmail.com>
2014-11-02 10:04:25 +0000

    Introducing command line options to specify seed and bitmap file

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 -- ghci -fno-ghci-sandbox DiamondSquare.hs
 
@@ -9,20 +10,93 @@ module Main
 where
 import Data.Word (Word8)
 import System.Random
-import Data.Array.Repa as R
+import qualified Data.Array.Repa as R
 import Data.Array.Repa.IO.BMP
 import HeightMap.Base (reify,heightMap,heightMap',unitHeightMap,unitHeightMap',getHeight)
 import HeightMap.Mesh ()
+import System.Console.GetOpt
+import System.Environment
+import System.IO
+import Data.Maybe ( fromMaybe, isJust, fromJust, isNothing )
+import Control.Monad
 
 --------------------------------------------------------------------------------
+
+-- Command line options processing
+
+data Options = Options {
+    optBmp       :: Maybe FilePath,
+    optHeightmap :: Maybe FilePath,
+    optSeed      :: Maybe Int,
+    optWidth     :: Int,
+    optHeight    :: Int
+} deriving Show
+
+defaultOptions   = Options {
+    optBmp       = Nothing,
+    optHeightmap = Nothing,
+    optSeed      = Nothing,
+    optWidth     = 512,
+    optHeight    = 512
+}
+
+options :: [OptDescr (Options -> Options)]
+options =
+    [ Option ['s']     ["seed"]       (OptArg seedOption      "SEED")  "use SEED",
+      Option ['b']     ["bitmap"]     (OptArg bmpOption       "FILE")  "output bitmap FILE",
+      Option ['m']     ["heightmap"]  (OptArg heightmapOption "FILE")  "output heightmap FILE" ]
+
+seedOption, bmpOption, heightmapOption :: Maybe String -> Options -> Options
+seedOption      val opts
+    | isNothing val = opts
+    | otherwise          = opts { optSeed      = Just seed }
+                           where seed :: Int = read $ fromJust val
+bmpOption       val opts = opts { optBmp       = val }
+heightmapOption val opts = opts { optHeightmap = val }
+
+compilerOpts :: [String] -> IO (Options, [String])
+compilerOpts argv =
+     case getOpt Permute options argv of
+         (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
+         (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+     where header = "Usage: <To be written>"
+
+-- Misc
 
 float2bytes :: Float -> (Word8,Word8,Word8)
 float2bytes v = (b,b,b) where b = (floor $ 255.0 * v) :: Word8
 
+getSeed :: Options -> IO Int
+getSeed opts = if isJust $ optSeed opts
+               then return (fromJust $ optSeed opts)
+               else randomIO
+
+-- Main
 
 main :: IO ()
 main = do
-  rg    <- getStdGen
+  args <- getArgs
+
+  putStrLn "Diamond square generator"
+  putStrLn "------------------------"
+
+  (opts,errors) <- compilerOpts args
+
+  when (isNothing (optBmp opts) && isNothing (optHeightmap opts)) $ ioError (userError "Nothing to do, please choose -b or -m")
+
+  seed <- getSeed opts
+  putStrLn      $ "Seed      : " ++ (show seed)
+  setStdGen $ mkStdGen seed
+  rg <- getStdGen
+
+  if isJust $ optBmp opts
+  then putStrLn $ "Bitmap    : '" ++ fromJust ( optBmp opts ) ++ "'"
+  else putStrLn $ "Bitmap    : <not produced>"
+
+  if isJust $ optHeightmap opts
+  then putStrLn $ "Heightmap : '" ++ fromJust ( optHeightmap opts ) ++ "'"
+  else putStrLn $ "Heightmap : <not produced>"
+
   seed1 <- randomRIO (0.0, 1.0) :: IO Float
   seed2 <- randomRIO (0.0, 1.0) :: IO Float
   seed3 <- randomRIO (0.0, 1.0) :: IO Float
@@ -31,5 +105,6 @@ main = do
   let hm = reify $ R.map (\p -> float2bytes $ getHeight p) 
                  $ unitHeightMap rg (240,240) seed1 seed2 seed3 seed4
 
-  --putStrLn $ show hm
-  writeImageToBMP "HM.bmp" hm
+  when (isJust (optBmp opts)) $ do
+    let filename = fromJust ( optBmp opts )
+    writeImageToBMP filename hm
